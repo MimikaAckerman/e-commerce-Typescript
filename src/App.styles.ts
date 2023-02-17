8e472b37f2ed538eba58a9d5539fca2b0bc44fb0
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-02-17 20:11:22 +0100

    holis

---
+++ src/App.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 70rem;
+  margin-top: 7rem;
+  margin-left: 1rem;
+  
+`;
+
+
+export const Navbar = styled.div`
+background-color: #06486E;
+width: 70rem;
+border-radius: 50px 50px 50px 50px;
+`
+
+export const H1navbar = styled.h1`
+color: white;
+margin-left: 2rem;
+
+`
+export const BtnCart = styled.button`
+margin-top: -8.5rem;
+position:absolute;
+margin-left: 73rem;
+`
---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,47 @@
+import { CartItemType } from '../App';
+import { Wrapper } from './CartItem.styles';
+
+
+type Props ={
+    item:CartItemType;
+    addToCart:(clickedItem:CartItemType) => void;
+    removeFromCart:(id:number) => void;
+};
+
+const CartItem: React.FC<Props> = ({item,addToCart,removeFromCart}) =>(
+<Wrapper>
+    <div>
+        <h3>{item.title}</h3>
+        <div className="information">
+            <p>Price: €{item.price}</p>
+            <p>Total: €{(item.amount * item.price).toFixed(2)}</p>
+        </div>
+        <div className="buttons">
+        <button
+          
+          onClick={() => removeFromCart(item.id)}
+        >
+          -
+        </button>
+        <p>{item.amount}</p>
+        <button
+        
+          onClick={() => addToCart(item)}
+        >
+          +
+        </button>
+        </div>
+    </div>
+    <img src={item.image} alt={item.title} />
+</Wrapper>
+)
+export default CartItem
+
+
+
+
+
+
+
+
+
